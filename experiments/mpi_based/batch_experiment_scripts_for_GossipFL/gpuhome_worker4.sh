eaca02823dff9eb4eb7c074b8d0ded19cf23f89d
wizard1203 <956234606@qq.com>
2023-11-10 15:58:57 +0800

    a;

--- experiments/mpi_based/batch_experiment_scripts_for_GossipFL/gpuhome_worker4.sh
+++ experiments/mpi_based/batch_experiment_scripts_for_GossipFL/gpuhome_worker4.sh
@@ -5,17 +5,16 @@ export project="gossipfl"
 
 export cluster_name="gpuhome"
 
-export PS_MPI_HOST="gpu17:5"
-export PS_GPU_MAPPINGS="gpu17:3,2"
+export PS_MPI_HOST="gpu16:5"
+export PS_GPU_MAPPINGS="gpu16:3,2"
 
-export FEDAVG_MPI_HOST="gpu17:3"
-export FEDAVG_GPU_MAPPINGS="gpu17:1,1"
+export FEDAVG_MPI_HOST="gpu16:3"
+export FEDAVG_GPU_MAPPINGS="gpu16:1,1"
 
-export GOSSIP_MPI_HOST="gpu17:4"
-export GOSSIP_GPU_MAPPINGS="gpu17:2,2"
+export GOSSIP_MPI_HOST="gpu16:4"
+export GOSSIP_GPU_MAPPINGS="gpu16:2,2"
 
-export wandb_record=False
-export level=DEBUG 
+
 
 export NWORKERS=4
 
@@ -25,18 +24,8 @@ export NWORKERS=4
 # export task="ptb"
 # export trainer_type="lstm"
 
-
-export dataset="mnist"
-export model="mnistflnet"
-export model_input_channels=1
-
-# export dataset="cifar10"
-# export model="cifar10flnet"
-# lrs=(0.04)
-
-# export model="resnet20"
-
-
+export dataset="cifar10"
+export model="cifar10flnet"
 
 # export dataset="shakespeare"
 # export model="rnn"
@@ -52,57 +41,25 @@ export lr_decay_rate=0.992
 # export partition_method='iid'
 export partition_method='hetero'
 export partition_alpha=0.5
-# export partition_alpha=10.0
-# partition_alpha_list=(1.0 10.0)
-# partition_alpha_list=(10.0)
-
-# export partition_method='noniid-#label2'
-# export partition_alpha=0.5
-
 
 # export lr=0.1
-# lrs=(0.01 0.01 0.3)
-# lrs=(0.03 0.1)
-# lrs=(0.03)
-lrs=(0.1)
-
-# export lr=0.1
+# lrs=(0.001 0.01 0.01 0.3)
+export lr=0.04
 
 # export Failure_chance=0.005
 
-# lr=$lr algorithm="PSGD" bash ./launch_mpi_based.sh
 # lr=$lr algorithm="PSGD" compression=eftopk compress_ratio=0.01      bash ./launch_mpi_based.sh
 
 # lr=$lr algorithm="FedAvg" compression=no  epochs=1   momentum=0.0          bash ./launch_mpi_based.sh
 # lr=$lr algorithm="FedAvg" compression=topk compress_ratio=0.01  epochs=1 \
 #      momentum=0.0          bash ./launch_mpi_based.sh
 # lr=$lr algorithm="DPSGD"             bash ./launch_mpi_based.sh
-
-# lr=$lr algorithm="SAPS_FL"           bash ./launch_mpi_based.sh
-# lr=$lr algorithm="CHOCO_SGD"         bash ./launch_mpi_based.sh
 # lr=$lr algorithm="DCD_PSGD"          bash ./launch_mpi_based.sh
-
-for lr in "${lrs[@]}"
-do
-    # lr=$lr partition_alpha=$partition_alpha algorithm="PSGD" bash ./launch_mpi_based.sh
-    # lr=$lr partition_alpha=$partition_alpha algorithm="PSGD" compression=eftopk compress_ratio=0.01      bash ./launch_mpi_based.sh
-    # lr=$lr partition_alpha=$partition_alpha algorithm="FedAvg" compression=no  epochs=1   momentum=0.0          bash ./launch_mpi_based.sh
-    # lr=$lr partition_alpha=$partition_alpha algorithm="FedAvg" compression=topk compress_ratio=0.25  epochs=1 \
-    #      momentum=0.0          bash ./launch_mpi_based.sh
-    # lr=$lr partition_alpha=$partition_alpha algorithm="DPSGD"             bash ./launch_mpi_based.sh
-    # lr=$lr partition_alpha=$partition_alpha algorithm="SAPS_FL" compress_ratio=0.1 \
-    #         bash ./launch_mpi_based.sh
-    # lr=$lr partition_alpha=$partition_alpha algorithm="CHOCO_SGD"  compress_ratio=0.1 \
-    #         bash ./launch_mpi_based.sh
-    lr=$lr partition_alpha=$partition_alpha algorithm="DCD_PSGD"          bash ./launch_mpi_based.sh
-done
+# lr=$lr algorithm="CHOCO_SGD"         bash ./launch_mpi_based.sh
+lr=$lr algorithm="SAPS_FL"           bash ./launch_mpi_based.sh
 
 
-# for lr in "${lrs[@]}"
-# do
-#      lr=$lr algorithm="SAPS_FL"           bash ./launch_mpi_based.sh
-#      lr=$lr algorithm="CHOCO_SGD"         bash ./launch_mpi_based.sh
-# done
+
 
 
 
--- model/build.py
+++ model/build.py
@@ -1,6 +1,5 @@
 import logging
 
-from model.linear.lr import LogisticRegression
 from model.cv.cnn import CNN_DropOut
 from model.cv.simplecnn_mnist import SimpleCNNMNIST 
 from model.cv.resnet_gn import resnet18
@@ -11,9 +10,6 @@ from model.cv.mobilenet_v3 import MobileNetV3
 from model.cv.efficientnet import EfficientNet
 from model.cv.mnistflnet import MnistFLNet
 from model.cv.cifar10flnet import Cifar10FLNet
-from model.nlp.rnn import RNN_OriginalFedAvg, RNN_StackOverFlow
-from model.nlp import lstm as lstmpy
-from model.nlp.lstman4 import create_net as LSTMAN4
 
 from model.cv.others import (ModerateCNNMNIST, ModerateCNN)
 
@@ -22,10 +18,7 @@ from model.cv.others import (ModerateCNNMNIST, ModerateCNN)
 def create_model(args, model_name, output_dim, **kargs):
     logging.info("create_model. model_name = %s, output_dim = %s" % (model_name, output_dim))
     model = None
-    if model_name == "lr" and args.dataset == "mnist":
-        logging.info("LogisticRegression + MNIST")
-        model = LogisticRegression(28 * 28, output_dim)
-    elif model_name == "simplecnn_mnist" and args.dataset in ["mnist", "fmnist"]:
+    if model_name == "simplecnn_mnist" and args.dataset in ["mnist", "fmnist"]:
         logging.info("simplecnn_mnist + MNIST or FMNIST")
         model = SimpleCNNMNIST(input_dim=(16 * 4 * 4), hidden_dims=[120, 84], output_dim=10)
     elif model_name == "mnistflnet" and args.dataset in ["mnist", "fmnist"]:
@@ -48,18 +41,6 @@ def create_model(args, model_name, output_dim, **kargs):
     elif model_name == "resnet18_gn":
         logging.info("ResNet18_GN")
         model = resnet18(pretrained=args.pretrained, num_classes=output_dim, group_norm=args.group_norm_num)
-    elif model_name == "rnn" and args.dataset == "shakespeare":
-        logging.info("RNN + shakespeare")
-        model = RNN_OriginalFedAvg(embedding_dim=args.lstm_embedding_dim, hidden_size=args.lstm_hidden_size)
-    elif model_name == "rnn" and args.dataset == "fed_shakespeare":
-        logging.info("RNN + fed_shakespeare")
-        model = RNN_OriginalFedAvg(embedding_dim=args.lstm_embedding_dim, hidden_size=args.lstm_hidden_size)
-    elif model_name == "lr" and args.dataset == "stackoverflow_lr":
-        logging.info("lr + stackoverflow_lr")
-        model = LogisticRegression(10004, output_dim)
-    elif model_name == "rnn" and args.dataset == "stackoverflow_nwp":
-        logging.info("CNN + stackoverflow_nwp")
-        model = RNN_StackOverFlow()
     elif model_name == "resnet20":
         model = resnet20(num_classes=output_dim)
     elif model_name == "resnet56":
@@ -89,14 +70,6 @@ def create_model(args, model_name, output_dim, **kargs):
         model = EfficientNet.from_name(
             model_name='efficientnet-b0', num_classes=output_dim)
         # model = EfficientNet.from_pretrained(model_name='efficientnet-b0')
-    elif model_name == 'lstman4':
-        model = LSTMAN4(datapath=args.an4_audio_path)
-    elif model_name == 'lstm':
-        model = lstmpy.lstm(vocab_size=kargs["vocab_size"], embedding_dim=args.lstm_embedding_dim, 
-                            batch_size=args.batch_size,
-                            num_steps=args.lstm_num_steps, dp_keep_prob=0.3)
-    elif model_name == 'lstmwt2':
-        model = lstmpy.lstmwt2(vocab_size=kargs["vocab_size"], batch_size=args.batch_size, dp_keep_prob=0.5)
     else:
         raise NotImplementedError
     return model
